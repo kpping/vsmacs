d90d24487b8c639052009c9eabf6639409198a3d
Krittanan Pingclasai <kp@kpping.me>
2021-08-03 23:24:01 +0700

    change function name

--- src/selection.ts
+++ src/selection.ts
@@ -2,18 +2,15 @@ import * as vscode from 'vscode';
 
 const selectModeMap = new Map();
 
-export function getActiveEditorId(editor = vscode.window.activeTextEditor): string {
+export function getEditorId(editor = vscode.window.activeTextEditor): string {
   return `${editor?.viewColumn || 0}${editor?.document.uri.toString()}`;
 }
 
-export function setSelectMode(
-  value: boolean,
-  editorId = getActiveEditorId()
-): Map<string, boolean> {
+export function setSelectMode(value: boolean, editorId = getEditorId()): Map<string, boolean> {
   return selectModeMap.set(editorId, value);
 }
 
-export function isSelectMode(editorId = getActiveEditorId()): boolean {
+export function isSelectMode(editorId = getEditorId()): boolean {
   if (!selectModeMap.has(editorId)) {
     setSelectMode(false, editorId);
   }
